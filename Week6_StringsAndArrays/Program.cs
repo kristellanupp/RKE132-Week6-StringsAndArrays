2b882ce0ab39765c3f7fe29c214efdef375c1737
Kristella Nupp <kristella.nupp@tktk.ee>
2023-11-23 00:36:16 +0200

    add week6 code

---
+++ Week6_StringsAndArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+ 
+//string trimmedString = hello.Trim();
+
+
+hello = hello.Trim(); //väärtuse "hello" ümberkirjutamine vahemälus
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+
+
+//int wordCounter = 1;  
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i])) //tagastab: true / false
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.);
